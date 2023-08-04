23a3b532e5e3ee1b89fd1c309e3afaf34ce4d052
hyg8trgt <99370366+hyg8trgt@users.noreply.github.com>
2023-08-04 18:18:05 -0400

    Add files via upload

---
+++ play-on-click.js
@@ -0,0 +1,32 @@
+AFRAME.registerComponent("play-on-click", {
+ //Add code here
+ schema:{
+isplaying:{type:"boolean",default:false}
+ },
+    init: function(){
+        this.video1=this.el.getAttribute("material").src
+        this.onClick=this.onClick.bind(this)
+    },
+    play:function () {
+        window.addEventListener("click",this.onClick);
+    },
+    onClick:function(evt){
+        if(!this.video1){
+            return
+        }
+        var isplaying=this.el.getAttribute("play-on-click").isplaying
+        this.el.object3D.visible=true
+        if(!isplaying){
+            this.el.setAttribute("play-on-click",{
+                isplaying:true
+            })
+            this.video1.play()
+        }else {
+            this.el.setAttribute("play-on-click",{
+                isplaying:false
+            })
+            this.video1.pause()
+        }
+    }
+
+});
